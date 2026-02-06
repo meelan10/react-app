e7fd2148bfd229d422b8cda781c17d72831cb5e4
Milan wagle <milanwagle73@gmail.com>
2026-02-06 22:10:23 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Meelan</h1>
+      <h1>Meelan wagle</h1>
       <p>Hare krishana hare ram </p>
 
       <div>
